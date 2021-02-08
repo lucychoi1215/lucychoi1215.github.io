e2f5b35bec701ce87782109a10de7aa2ca5c1117
lucychoi1215 <56735732+lucychoi1215@users.noreply.github.com>
2021-02-08 22:03:59 +0400

    final

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let actressInfo1 = () => {
+  document.getElementById("name1").innerText = "Lucy Choi, Class 2021"
+}
+
+
+let actressInfo2 = () => {
+  document.getElementById("name2").innerText = "Noora Shuaib, Class 2022"
+}
+
+let actressInfo3 = () => {
+  document.getElementById("name3").innerText = "Liyan, Class 2022"
+}
+
+let actressInfo4 = () => {
+  document.getElementById("name4").innerText = "Maira, Class 2021"
+}
+
+let detailsContainer = document.getElementById("button");
+
+detailsContainer.addEventListener('click',actressInfo1)
+
+detailsContainer.addEventListener('click',actressInfo2)
+
+detailsContainer.addEventListener('click',actressInfo3)
+
+detailsContainer.addEventListener('click',actressInfo4)
